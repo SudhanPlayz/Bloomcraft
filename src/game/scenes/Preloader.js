a23f34338ed82b812705a9b2c12258bb9daf67c1
Sudhan <sudhanplayz@gmail.com>
2024-07-26 15:58:29 +0530

    refactor: Remove unnecessary code in Preloader.js and App.jsx

--- src/game/scenes/Preloader.js
+++ src/game/scenes/Preloader.js
@@ -6,9 +6,6 @@ export class Preloader extends Scene {
     }
 
     init() {
-        //  We loaded this image in our Boot Scene, so we can display it here
-        this.add.image(512, 384, 'background');
-
         //  A simple progress bar. This is the outline of the bar.
         this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
 
@@ -17,15 +14,12 @@ export class Preloader extends Scene {
 
         //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
         this.load.on('progress', (progress) => {
-
             //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
             bar.width = 4 + (460 * progress);
-
         });
     }
 
     preload() {
-        //  Load the assets for the game - Replace with your own assets
         this.load.setPath('assets');
 
         this.load.image('logo', 'logo.png');
